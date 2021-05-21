2be26415dd775ea42305f11bee0e760779a4d3fd
gwenjo <jo.gwen@hotmail.com>
2021-05-21 20:57:04 +0000

    Create base template

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_tasks")
-def get_tasks():
-    tasks = mongo.db.tasks.find()
-    return render_template("tasks.html", tasks=tasks)
+@app.route("/get_recipe")
+def get_recipe():
+    recipe = mongo.db.recipe.find()
+    return render_template("recipe.html", recipe=recipe)
 
 
 if __name__ == "__main__":
