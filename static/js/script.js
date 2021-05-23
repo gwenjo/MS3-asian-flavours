c97a668249f1bc29491f35469040eb120b83f0f6
gwenjo <jo.gwen@hotmail.com>
2021-05-23 21:12:54 +0000

    modified requirements.txt

--- static/js/script.js
+++ static/js/script.js
@@ -2,12 +2,5 @@ $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
     $(".collapsible").collapsible();
     $(".tooltipped").tooltip();
-    $(".datepicker").datepicker({
-        format: "dd mmmm, yyyy",
-        yearRange: 3,
-        showClearBtn: true,
-        i18n: {
-            done: "Select"
-        }
-    });
+    
 });
